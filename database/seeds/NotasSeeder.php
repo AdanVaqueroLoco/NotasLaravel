7204d818b5cf6d42f5428d81dd70e537500b9fd1
Adan905 <mexicanoestupido69@hotmail.com>
2022-03-03 20:47:07 -0700

    Actividad complementaria

--- database/seeds/NotasSeeder.php
+++ database/seeds/NotasSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
 
 class NotasSeeder extends Seeder
 {
@@ -13,8 +15,28 @@ class NotasSeeder extends Seeder
     public function run()
     {
         DB::table('notas')->insert([
-            'titulo' => 'Mi primera nota', 
-            'contenido' => 'Contenido de prueba',
+            'titulo' => '¿Para que sirve Composer', 
+            'contenido' => 'Con Composer podemos instalar y actualizar frameworks como Laravel o Symfony,
+                        así como componentes para fenerar PDF, procesar pagos con tarjetas, manipular imágenes y mucho más',
+        ]);
+        DB::table('notas')->insert([
+            'titulo' => 'Instalación de Laravel', 
+            'contenido' => File::get(__DIR__.'/notas/install.html'),
+        ]);
+
+        DB::table('notas')->insert([
+            'titulo' => 'Rutas y JSON', 
+            'contenido' => File::get(__DIR__.'/notas/routes.html'),
+        ]);
+
+        DB::table('notas')->insert([
+            'titulo' => 'Front Controller', 
+            'contenido' => File::get(__DIR__.'/notas/frontcontroller.html'),
+        ]);
+
+        DB::table('notas')->insert([
+            'titulo' => 'Cambia el formato de parámetros dinámicos', 
+            'contenido' => File::get(__DIR__.'/notas/format.html'),
         ]);
     }
 }

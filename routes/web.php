0beaab7a05820711c31a27e021111d4ee5dfdaae
Adan905 <mexicanoestupido69@hotmail.com>
2022-03-03 21:24:06 -0700

    Clase 4 de Marzo

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,18 @@
 */
 
 Route::get('/', function () {
-    $notas = [
-        'Mi primera nota',
-        'Mi segunda nota',
-        'Mi tercera nota',
-        'Mi cuarta nota',
-    ];
+    $notas = DB::table('notas')->get();
+
 
     return view('notas', ['notas' => $notas]);
 })->name('listar');
+
+Route::get('notas', function () {
+    $notas = DB::table('notas')->get();
+
+
+    return view('notas', ['notas' => $notas]);
+})->name('notas.index');
 
 Route::get('notas/{id}', function ($id) {
     return 'Aquí veremos el detalle de la nota: '.$id;
@@ -29,6 +32,13 @@ Route::get('notas/{id}', function ($id) {
 Route::get('agregar', function () {
     return view('agregar');
 })->name('nuevanota');
+
+Route::get('notas/{id}/editar', function ($id) {
+    $nota = DB::table('notas')
+    ->where('id', $id)
+    ->first();
+    return 'Aquí se van a editar las notas'.$id;
+})->name('notas.edit');
 
 Route::get('editar', function () {
     return view('editar');
